35fd38be43db6791253f9a29de638e2f9f0c0b7e
son_mac <smk2591@naver.com>
2025-10-30 16:09:10 +0900

    feat: user 회원가입 서비스 개발

--- src/main/java/com/kt/service/UserService.java
+++ src/main/java/com/kt/service/UserService.java
@@ -1,5 +1,7 @@
 package com.kt.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import com.kt.domain.User;
@@ -15,10 +17,16 @@ public class UserService {
 
 	public void create(UserCreateRequest request) {
 		var newUser = new User(
+			userRepository.selectMaxId() + 1,
 			request.loginId(),
 			request.password(),
 			request.name(),
-			request.birthday()
+			request.email(),
+			request.mobile(),
+			request.gender(),
+			request.birthday(),
+			LocalDateTime.now(),
+			LocalDateTime.now()
 		);
 
 		userRepository.save(newUser);
