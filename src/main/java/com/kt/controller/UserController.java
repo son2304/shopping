0399dac556c50e97079ea1e0d31bdd892005f571
son_mac <smk2591@naver.com>
2025-11-03 09:49:00 +0900

    feat: 아이디 중복체크 api 개발

--- src/main/java/com/kt/controller/UserController.java
+++ src/main/java/com/kt/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.kt.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,22 +15,34 @@ import com.kt.service.UserService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "유저", description = "유저 관련 API")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@ApiResponses(value = {
+	@ApiResponse(responseCode = "400", description = "유효성 검사 실패"),
+	@ApiResponse(responseCode = "500", description = "서버 에러 - 백엔드에 바로 문의 바랍니다.")
+})
 public class UserController {
 	private final UserService userService;
 
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "400", description = "유효성 검사 실패"),
-		@ApiResponse(responseCode = "500", description = "서버 에러 - 백엔드에 바로 문의 바랍니다.")
-	})
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void create(@Valid @RequestBody UserCreateRequest request) {
 		userService.create(request);
+	}
+
+	// /users/duplicate-login-id?loginId=s1234
+	// IllegalArgumentException 발생 시 에러 400
+	// GET에서 쓰는 queryString
+	// @RequestParam의 속성은 기본이 required=true
+	@GetMapping("/users/duplicate-login-id")
+	@ResponseStatus(HttpStatus.OK)
+	public Boolean isDuplicateLoginId(@RequestParam(required = false) String loginId) {
+		return userService.isDuplicateLoginId(loginId);
 	}
 }
